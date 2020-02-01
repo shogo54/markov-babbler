cb9ba5c840883e055df5568989974094829aa32e
shogo54 <sakiyama@knox.edu>
2020-01-31 22:00:40 -0600

    Add gitignore and chage the location of make_docs.py

--- docs/make_docs.py
+++ docs/make_docs.py
@@ -2,8 +2,8 @@ import subprocess
 
 
 def run():
-    cmd_api = "sphinx-apidoc -f -o ./docs ./"
-    cmd_doc = "sphinx-build -b html ./docs ./docs/_build"
+    cmd_api = "sphinx-apidoc -f -o . ."
+    cmd_doc = "sphinx-build -b html . ./_build"
 
     for cmd in [cmd_api, cmd_doc]:
         result = res = subprocess.Popen(cmd, shell=True, universal_newlines=False)
